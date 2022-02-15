cf8d0d7664881f765d2cecd285733be75b9c28f2
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2022-02-15 15:26:17 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining_lab2.1/Main.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB.Mechanical;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_lab2._1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var ducts = new FilteredElementCollector(doc)
+                            .OfClass(typeof(Duct))
+                            .Cast<Duct>()
+                            .ToList();
+
+            TaskDialog.Show("Количество воздуховодов", ducts.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
